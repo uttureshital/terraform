d2c8f9cc0fcbba46e3f49888f58daf94e81e219a
uttureshital <shitalutture1@gmail.com>
2024-02-14 22:09:37 +0530

    add file

---
+++ instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "my-instance" {
+  ami           = "ami-0449c34f967dbf18a"
+  instance_type = "t2.micro"
+  key_name = demo
+  vpc_security_group_ids = "sg-0b83a9a554862d712"
+  tags = {
+    Name = "demo-instance"
+  }
+}
