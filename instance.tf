470a10782437e31657147f9f0bde55eee38317bf
uttureshital <shitalutture1@gmail.com>
2024-02-14 22:14:14 +0530

    add changes

--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my-instance" {
   ami           = "ami-0449c34f967dbf18a"
   instance_type = "t2.micro"
-  key_name = demo
+  key_name = "demo"
   vpc_security_group_ids = "sg-0b83a9a554862d712"
   tags = {
     Name = "demo-instance"
