baf23ac72c8840b735705e53a06182cac5e21ae9
uttureshital <shitalutture1@gmail.com>
2024-02-22 22:39:48 +0530

    asd

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -57,4 +57,134 @@ resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOn
   role       = aws_iam_role.node-role.name
 }
 
-#
+#create vpc 
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block       = "10.0.0.0/20"
+  tags = {
+    Name = "var.my-vpc"
+  }
+} 
+
+#create subnet 
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/11"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "var.public-subnet"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/17"
+
+  tags = {
+    Name = "var.private-subnet"
+  }
+}
+#create internet gateway
+
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "var.my-igw"
+  }
+}
+
+#create rounte table
+
+resource "aws_route" "my_route" {
+  route_table_id            = aws_vpc.my-vpc.default_route_table_id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my-igw.id
+}
+
+#create security group 
+
+resource "aws_security_group" "my-sg" {
+  name        = "var.my-sg"
+  description = "allow inbond rule"
+  vpc_id      = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "var.my-sg"
+  }
+}
+  ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "-1"
+        from_port = 0
+        to_port = 0
+    }
+ 
+  egress {
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "-1"
+        from_port = 0
+        to_port = 0
+    }
+  
+#create eks cluster
+
+resource "aws_eks_cluster" "my-cluster" {
+  name     = "var.my-cluster"
+  role_arn = aws_iam_role.cluster-role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.private.id, aws_subnet.public.id]
+  }
+
+  
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+  ]
+}
+
+#create node group
+
+resource "aws_eks_node_group" "my-node-group" {
+  cluster_name    = aws_eks_cluster.my-cluster.name
+  node_group_name = "shital-node"
+  node_role_arn   = aws_iam_role.node-role.arn
+  subnet_ids      = [
+     aws_subnet.public.id,
+     aws_subnet.private.id
+    ]
+
+ capacity_type = "ON_DEMAND"
+  instance_types = ["t3.micro"]
+  scaling_config {
+    desired_size = 2
+    min_size = 1
+    max_size = 4
+  }
+  update_config {
+    max_unavailable = 1
+  }
+  depends_on = [ 
+    aws_iam_role_policy_attachment.my-node-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.my-node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.my-node-AmazonEKS_CNI_Policy
+  ]
+}
+
+resource "aws_nat_gateway" "my-nat-gateway" {
+  allocation_id = aws_eip.my_eip.id
+  subnet_id     = aws_subnet.private.id
+}
+
+# Elastic IP for NAT Gateway
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0014ce3e52359afbd"  
+  instance_type = "t3.micro"                
+  subnet_id     = aws_subnet.public.id
+}
+
+resource "aws_eip" "my_eip" {
+  instance = aws_instance.my_instance.id
+}
--- eks-cluster/variable.tf
+++ eks-cluster/variable.tf
@@ -3,3 +3,22 @@ variable "cluster-role"
 
 variable "node-role"    
     default = "noderole"
+
+variable "my-vpc"
+    default = "eks-vpc" 
+
+variable "my-vpc"
+    default = "eks-igw"    
+
+variable "public-subnet"
+    default = "public"
+
+variable "private-subnet"
+    default = "private"
+
+variable "my-sg"
+    default = "eks-sg"
+
+variable "my-cluster"
+    default = "eks-cluster"
+
