34734ae4af43bf440c343cc230f67878a1f559a4
uttureshital <shitalutture1@gmail.com>
2024-02-25 00:22:06 +0530

    asdfgh

--- jenkins/jenkins.tf
+++ jenkins/jenkins.tf
@@ -16,7 +16,6 @@ resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-southeast-2a"
-  map_public_ip_on_launch = true
   tags = {
     Name = "public"
     }
