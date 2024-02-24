64b574f44bffe8c650f240829b87357ad8a48ad8
uttureshital <shitalutture1@gmail.com>
2024-02-25 01:01:16 +0530

    sss

--- jenkins/jenkins.tf
+++ jenkins/jenkins.tf
@@ -61,7 +61,7 @@ resource "aws_instance" "instance" {
         
     }
     subnet_id = aws_subnet.my_subnet.id
-    /*
+    
     user_data = <<EOF
                  #!/bin/bash
 
@@ -82,24 +82,6 @@ resource "aws_instance" "instance" {
                  sudo systemctl enable jenkins
 
                  EOF
-    */
-
-    user_data = <<EOF
-                 #!/bin/bash
-                 sudo apt update -y
-                 sudo apt install fontconfig openjdk-17-jre -y
-                 java -version
-                 openjdk version "17.0.8" 2023-07-18
-                 OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
-                 OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
-                 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-                 https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-                 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-                 https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-                 /etc/apt/sources.list.d/jenkins.list > /dev/null
-                 sudo apt-get update -y
-                 sudo apt-get install jenkins -y
-                 EOF
-                
-        
 }
+
+   
