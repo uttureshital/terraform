ca396c0017ec92eecec94e017b84f05de0432a27
uttureshital <shitalutture1@gmail.com>
2024-02-25 22:05:08 +0530

    asdf

--- jenkins/jenkins.tf
+++ jenkins/jenkins.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "my_sg" {
 #creating instance
 
 resource "aws_instance" "instance" {
-    ami = "ami-04f5097681773b989"
+    ami = "ami-0e5f882be1900e43b"
     instance_type = "t2.micro"
     key_name = "shital"
     vpc_security_group_ids = [aws_security_group.my_sg.id]
